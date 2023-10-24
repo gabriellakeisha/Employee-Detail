7f5a8c1917644e5c8a24e9c533b3128aa4f190a2
Gabriella Keisha Andini <123333260+gabriellakeisha@users.noreply.github.com>
2023-10-24 17:40:13 +0100

    Add files via upload

---
+++  employee_data.py
@@ -0,0 +1,316 @@
+employees = [
+    {
+        "first_name":"Jose",
+        "last_name":"Lopez",
+        "email":"joselopez0944@example.com",
+        "age":25,
+        "job_title":"Project Manager",
+        "years_of_experience":1,
+        "salary":8500,
+        "department":"Product"
+    },
+    {
+        "first_name":"Diane",
+        "last_name":"Carter",
+        "email":"dianecarter1228@example.com",
+        "age":26,
+        "job_title":"Machine Learning Engineer",
+        "years_of_experience":2,
+        "salary":7000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Shawn",
+        "last_name":"Foster",
+        "email": None,
+        "age":37,
+        "job_title":"Customer Service Rep",
+        "years_of_experience":14,
+        "salary":17000,
+        "department":"Business"
+    },
+    {
+        "first_name":"Brenda",
+        "last_name":"Fisher",
+        "email":"brendafisher3185@example.com",
+        "age":31,
+        "job_title":"Web Developer",
+        "years_of_experience":8,
+        "salary":10000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Sean",
+        "last_name":"Hunter",
+        "email": None,
+        "age":35,
+        "job_title":"Project Manager",
+        "years_of_experience":11,
+        "salary":14500,
+        "department":"Product"
+    },
+    {
+        "first_name":"Joshua",
+        "last_name":"Jacobs",
+        "email":"joshuajacobs5904@example.com",
+        "age":28,
+        "job_title":"Project Manager",
+        "years_of_experience":3,
+        "salary":10500,
+        "department":"Business"
+    },
+    {
+        "first_name":"Brianna",
+        "last_name":"Marshall",
+        "email":None,
+        "age":33,
+        "job_title":"Machine Learning Engineer",
+        "years_of_experience":10,
+        "salary":11000,
+        "department":"Product"
+    },
+    {
+        "first_name":"John",
+        "last_name":"Tate",
+        "email":"johntate7881@example.com",
+        "age":33,
+        "job_title":"Mobile Developer",
+        "years_of_experience":10,
+        "salary":11000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Jillian",
+        "last_name":"Byrd",
+        "email":None,
+        "age":34,
+        "job_title":"Business Analyst",
+        "years_of_experience":10,
+        "salary":11000,
+        "department":"Business"
+    },
+    {
+        "first_name":"Melanie",
+        "last_name":"Sharp",
+        "email":"melaniesharp9256@example.com",
+        "age":41,
+        "job_title":"Web Developer",
+        "years_of_experience":15,
+        "salary":14500,
+        "department":"Product"
+    },
+    {
+        "first_name":"Brandy",
+        "last_name":"Mckee",
+        "email":None,
+        "age":37,
+        "job_title":"Marketing Manager",
+        "years_of_experience":14,
+        "salary":14000,
+        "department":"Business"
+    },
+    {
+        "first_name":"Robert",
+        "last_name":"Simpson",
+        "email":"robertsimpson11778@example.com",
+        "age":36,
+        "job_title":"Marketing Manager",
+        "years_of_experience":12,
+        "salary":15000,
+        "department":"Business"
+    },
+    {
+        "first_name":"George",
+        "last_name":"Mckenzie",
+        "email":"georgemckenzie12384@example.com",
+        "age":28,
+        "job_title":"Machine Learning Engineer",
+        "years_of_experience":4,
+        "salary":10000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Joseph",
+        "last_name":"Smith",
+        "email":None,
+        "age":40,
+        "job_title":"Machine Learning Engineer",
+        "years_of_experience":14,
+        "salary":14000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Dana",
+        "last_name":"Crawford",
+        "email":"danacrawford14310@example.com",
+        "age":32,
+        "job_title":"Project Manager",
+        "years_of_experience":8,
+        "salary":12000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Christopher",
+        "last_name":"Benson",
+        "email":None,
+        "age":29,
+        "job_title":"Web Developer",
+        "years_of_experience":5,
+        "salary":7500,
+        "department":"Product"
+    },
+    {
+        "first_name":"Nicole",
+        "last_name":"Smith",
+        "email":"nicolesmith16360@example.com",
+        "age":26,
+        "job_title":"Designer",
+        "years_of_experience":4,
+        "salary":10000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Peter",
+        "last_name":"Jimenez",
+        "email":"peterjimenez17791@example.com",
+        "age":28,
+        "job_title":"UX Designer",
+        "years_of_experience":3,
+        "salary":6500,
+        "department":"Business"
+    },
+    {
+        "first_name":"Sergio",
+        "last_name":"Boyle",
+        "email":"sergioboyle18425@example.com",
+        "age":31,
+        "job_title":"Tester",
+        "years_of_experience":6,
+        "salary":9000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Brianna",
+        "last_name":"Moss",
+        "email":None,
+        "age":31,
+        "job_title":"Designer",
+        "years_of_experience":5,
+        "salary":10500,
+        "department":"Product"
+    },
+    {
+        "first_name":"Taylor",
+        "last_name":"Garner",
+        "email":"taylorgarner20196@example.com",
+        "age":32,
+        "job_title":"Machine Learning Engineer",
+        "years_of_experience":6,
+        "salary":11000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Michael",
+        "last_name":"Padilla",
+        "email":"michaelpadilla21381@example.com",
+        "age":29,
+        "job_title":"Customer Service Rep",
+        "years_of_experience":5,
+        "salary":9500,
+        "department":"Business"
+    },
+    {
+        "first_name":"Yvette",
+        "last_name":"Walker",
+        "email":None,
+        "age":26,
+        "job_title":"Designer",
+        "years_of_experience":2,
+        "salary":7000,
+        "department":"Product"
+    },
+    {
+        "first_name":"Kristina",
+        "last_name":"Pena",
+        "email":"kristinapena23750@example.com",
+        "age":34,
+        "job_title":"Business Analyst",
+        "years_of_experience":11,
+        "salary":12500,
+        "department":"Business"
+    }
+]
+
+#1.Print the name of the person with the highest salary
+highest_salary = 0
+highest_salary_name = ""
+
+for employee in employees:
+    if employee["salary"] > highest_salary:
+        highest_salary = employee["salary"]
+        highest_salary_name = f"{employee['first_name']} {employee['last_name']}"
+print(f"The person with the highest salary is: {highest_salary_name}")
+
+#2.Print the combined years of experience of all employees
+total_experience = 0
+
+for employee in employees:
+    total_experience += employee["years_of_experience"]
+
+print(f"The combined years of experience of all employees is: {total_experience} years")
+
+#3.Collect details of employees without an email address into a new list
+employees_without_email = []
+
+for employee in employees:
+    if employee["email"] is None:
+        employees_without_email.append(employee)
+
+print("Details of employees without an email address:")
+for employee in employees_without_email:
+    print(employee)
+
+#4.Compare salaries of Product department and Business department
+product_salary_total = 0
+business_salary_total = 0
+
+for employee in employees:
+    if employee["department"] == "Product":
+        product_salary_total += employee["salary"]
+    elif employee["department"] == "Business":
+        business_salary_total += employee["salary"]
+
+if product_salary_total > business_salary_total:
+    print("Product department costs more for the company.")
+else:
+    print("Business department costs more for the company.")
+
+#5.Calculate the average salary for people over 30 years of age
+total_salary_over_30 = 0
+count_over_30 = 0
+
+for employee in employees:
+    if employee["age"] > 30:
+        total_salary_over_30 += employee["salary"]
+        count_over_30 += 1
+
+if count_over_30 > 0:
+    average_salary_over_30 = total_salary_over_30 / count_over_30
+    print(f"The average salary for people over 30 years of age is: {average_salary_over_30:.2f}")
+else:
+    print("There are no employees over 30 years of age.")
+
+#6.Create a new dictionary to count employees by job title
+job_title_counts = {}
+
+for employee in employees:
+    job_title = employee["job_title"]
+    if job_title in job_title_counts:
+        job_title_counts[job_title] += 1
+    else:
+        job_title_counts[job_title] = 1
+
+print("Number of employees by job title:")
+for job_title, count in job_title_counts.items():
+    print(f"{job_title}: {count}")
+
